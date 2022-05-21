f40f0d3bd3e22c306bb0570592403025a80e0d22
chris <kakaxi77749@gmail.com>
2022-05-21 16:11:18 +0800

    udp

--- udp_server.js
+++ udp_server.js
@@ -13,12 +13,14 @@ socket.on('message', (msg, rinfo) => {
 socket.on('listening', () => {
   const address = socket.address()
   console.log(`server listening ${address.address}:${address.port}`)
+  const client = dgram.createSocket('udp4')
+  const host = 'dht.transmissionbt.com'
+  const msg = 'd1:ad2:id20:abcdefghij0123456789e1:q4:ping1:t2:aa1:y1:qe'
+  client.send(msg,0,msg.length,6881,host,()=>{
+      console.log('send',msg,'to',host,'success')
+  })
 })
 
 socket.bind(6881)
 
-const host = '212.129.33.59'
-const msg = 'd1:ad2:id20:abcdefghij01234567896:target20:mnopqrstuvwxyz123456e1:q9:find_node1:t2:aa1:y1:qe'
-socket.send(msg,0,msg.length,6881,host,()=>{
-    console.log('send',msg,'to',host,'success')
-})
+
